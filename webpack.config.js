63f95ff91ce0600a1ebd18a6b7bb5f3d5d18d1df
Kevin Caro Silva <>
2020-03-31 19:30:04 -0400

    Cleanup slides

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
 const path = require("path");
 
 const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
 const CopyPlugin = require('copy-webpack-plugin');
 const PrettierPlugin = require("prettier-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const revealjsWrapperWebpack = require("react-revealjs-wrapper/src/webpack-html-template-config.js");
 const merge = require('webpack-merge');
 
@@ -19,10 +24,20 @@ module.exports = merge(revealjsWrapperWebpack, {
   module: {
     rules: [
       {
+        test: /reveal\.js$/,
+        use: [ 'script-loader' ]
+      },
+      {
         test: /\.[jt]sx?$/,
         use: "babel-loader",
         exclude: /node_modules/
       },
+      // {
+      //   // I want to uglify with mangling only app files, not thirdparty libs
+      //   test: /\.js$/,
+      //   exclude: /node_modules\/reveal.js\/js\/reveal\.js/,
+      //   use: 'uglify-loader',
+      // },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
@@ -39,6 +54,17 @@ module.exports = merge(revealjsWrapperWebpack, {
       {from: 'node_modules/reveal.js/plugin', to: 'plugin'},
       {from: 'src/assets', to: 'assets'},
     ]),
-    new PrettierPlugin()
-  ]
+    new PrettierPlugin(),
+      // new webpack.IgnorePlugin({resourceRegExp: /^reveal\.js$/})
+  ],
+  optimization: {
+    minimizer: [
+      // new UglifyJsPlugin({
+      //   exclude: /node_modules/,
+      // }),
+      new TerserPlugin({
+        exclude: /node_modules\/reveal\.js/,
+      })
+    ],
+  },
 });
